143a91918b4fcec9e5ac3810007baaf3773453ee
ThomatMorningsideHQ <thomas.morris@morningsidehq.com>
2024-11-18 10:50:25 -0500

    Fixed issues in test-utils causing deployment failure.

--- src/test/test-utils.tsx
+++ src/test/test-utils.tsx
@@ -1,4 +1,4 @@
-import { ReactElement } from 'react'
+import { ReactElement, ReactNode } from 'react'
 import { render, RenderOptions } from '@testing-library/react'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
@@ -6,15 +6,15 @@ const queryClient = new QueryClient({
   defaultOptions: { queries: { retry: false } },
 })
 
-const Wrapper = ({ children }: { children: ReactElement }) => (
-  <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
-)
+const Wrapper = ({ children }: { children: ReactNode }) => <>{children}</>
 
 // All the providers you need for tests can go here : Theme, Redux, etc.
 const customRender = (
   ui: ReactElement,
   options?: Omit<RenderOptions, 'wrapper'>,
-) => render(ui, { wrapper: Wrapper, ...options })
+) => {
+  return render(ui, { wrapper: Wrapper, ...options })
+}
 
 export * from '@testing-library/react'
 export { default as userEvent } from '@testing-library/user-event'
